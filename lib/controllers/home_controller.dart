e2b3164a224b6e5efe1d2a369ec4aadbed10348a
Kapil Ramkhiladi Singh <singhkapil708@gmail.com>
2022-08-12 22:34:11 +0530

    Home screen Get data by collection

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,8 +1,9 @@
 import 'package:tulsiresin/common/array/product.dart';
 import 'package:tulsiresin/config.dart';
+import 'package:tulsiresin/mixins/shopify_mixin.dart';
 import 'package:tulsiresin/models/product.dart';
 
-class HomeController extends GetxController {
+class HomeController extends GetxController with ShopifyMixin {
   List<ProductModel> recentView = [];
   List<ProductModel> womenCollection = [];
 
@@ -13,5 +14,14 @@ class HomeController extends GetxController {
     womenCollection = geRecentProducts;
     update();
     super.onReady();
+  }
+
+  Future<List<ProductModel>> getProductDataByCollectionId(String categoryId) async {
+    try {
+      List<ProductModel> data = await shopifyService.fetchProductsByCategory(categoryId: categoryId, page: 1, limit: 5) ?? [];
+      return data;
+    } on Exception catch (e) {
+      return [];
+    }
   }
 }
--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,6 +1,8 @@
+import 'package:tulsiresin/app_services/config.dart';
 import 'package:tulsiresin/common/array/product.dart';
 import 'package:tulsiresin/controllers/dashboard_controller.dart';
 import 'package:tulsiresin/controllers/home_controller.dart';
+import 'package:tulsiresin/models/product.dart';
 import 'package:tulsiresin/views/home/widgets/banner_list.dart';
 import 'package:tulsiresin/views/home/widgets/common_title.dart';
 import 'package:tulsiresin/views/home/widgets/recent_view_list.dart';
@@ -9,97 +11,155 @@ import '../../config.dart';
 
 class Home extends StatelessWidget {
   final homeCtrl = Get.put(HomeController());
-   Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<HomeController>(
-      builder: (_) {
-        return Scaffold(
+    List collections = config['home']['collections'] ?? [];
+    return GetBuilder<HomeController>(builder: (_) {
+      return Scaffold(
+        backgroundColor: appCtrl.appTheme.surface,
+        appBar: AppBar(
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          centerTitle: false,
           backgroundColor: appCtrl.appTheme.surface,
-          appBar: AppBar(
-            elevation: 0,
-            automaticallyImplyLeading: false,
-            centerTitle: false,
+          title: Image.asset(
+            imageAssets.logo,
+            height: Sizes.s50,
+            width: Sizes.s100,
+            fit: BoxFit.contain,
+          ),
+          actions: [
+            GetBuilder<DashboardController>(builder: (ctrl) {
+              return SvgPicture.asset(
+                svgAssets.search,
+                height: Sizes.s20,
+              ).gestures(onTap: () {
+                ctrl.selectedIndex = 2;
+                ctrl.update();
+                homeCtrl.update();
+              }).paddingOnly(right: Insets.i15);
+            })
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              // banner list
+              const BannerList(),
 
-            backgroundColor: appCtrl.appTheme.surface,
-            title: Image.asset(
-              imageAssets.logo,
-              height: Sizes.s50,
-              width: Sizes.s100,
-              fit: BoxFit.contain,
-            ),
-            actions: [
-              GetBuilder<DashboardController>(
-                builder: (ctrl) {
-                  return SvgPicture.asset(
-                    svgAssets.search,
-                    height: Sizes.s20,
-                  ).gestures(onTap: (){
-                    ctrl.selectedIndex =2;
-                    ctrl.update();
-                    homeCtrl.update();
-                  }).paddingOnly(right: Insets.i15);
-                }
-              )
+              ...collections.map((collection) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CommonTitle(title: collection['name'] ?? ''),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          FutureBuilder(
+                            future: homeCtrl.getProductDataByCollectionId(collection['category']),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState == ConnectionState.done) {
+                                // If we got an error
+                                if (snapshot.hasError) {
+                                  return Center(
+                                    child: Text(
+                                      '${snapshot.error} occurred',
+                                      style: const TextStyle(fontSize: 18),
+                                    ),
+                                  );
+
+                                  // if we got our data
+                                } else if (snapshot.hasData) {
+                                  // Extracting data from snapshot object
+                                  final data = snapshot.data as List<ProductModel>;
+                                  return SingleChildScrollView(
+                                    scrollDirection: Axis.horizontal,
+                                    child: Row(children: [
+                                      ...data.asMap().entries.map((e) {
+                                        return RecentViewListCard(
+                                          data: e.value,
+                                          index: e.key,
+                                          onTap: () => Get.toNamed(routeName.productDetail, arguments: e.value),
+                                          heartIconTap: () {},
+                                        );
+                                      }).toList(),
+                                    ]),
+                                  );
+                                }
+                              }
+                              return const CircularProgressIndicator();
+                            },
+                          ),
+                          /*...geRecentProducts.asMap().entries.map((e) {
+                            return RecentViewListCard(
+                              data: e.value,
+                              index: e.key,
+                              onTap: () => Get.toNamed(routeName.productDetail, arguments: e.value),
+                              heartIconTap: () {
+                                */ /*e.value.isFav = !e.value.isFav!;
+                            homeCtrl.update();*/ /*
+                              },
+                            );
+                          })*/
+                        ],
+                      ).marginOnly(left: Insets.i20),
+                    ),
+                  ],
+                );
+              }).toList(),
+
+              //recent View
+              /*CommonTitle(
+                title: CommonFonts().recentView,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    ...geRecentProducts.asMap().entries.map((e) {
+                      return RecentViewListCard(
+                        data: e.value,
+                        index: e.key,
+                        onTap: () => Get.toNamed(routeName.productDetail, arguments: e.value),
+                        heartIconTap: () {
+                          */ /*e.value.isFav = !e.value.isFav!;
+                            homeCtrl.update();*/ /*
+                        },
+                      );
+                    })
+                  ],
+                ).marginOnly(left: Insets.i20),
+              ),*/
+
+              //recent View
+              /*CommonTitle(
+                title: CommonFonts().womenCollection,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    ...geRecentProducts1.asMap().entries.map((e) {
+                      return RecentViewListCard(
+                        data: e.value,
+                        index: e.key,
+                        onTap: () => Get.toNamed(routeName.productDetail, arguments: e.value),
+                        heartIconTap: () {
+                          */ /* e.value.isFav = !e.value.isFav!;
+                            homeCtrl.update();*/ /*
+                        },
+                      );
+                    })
+                  ],
+                ).marginOnly(left: Insets.i20),
+              ),*/
             ],
-          ),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                // banner list
-                const BannerList(),
-
-                //recent View
-                 CommonTitle(
-                  title: CommonFonts().recentView,
-                ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      ...geRecentProducts.asMap().entries.map((e) {
-                        return RecentViewListCard(
-                          data: e.value,
-                          index: e.key,
-                          onTap: ()=> Get.toNamed(routeName.productDetail,arguments: e.value),
-                          heartIconTap: (){
-                            /*e.value.isFav = !e.value.isFav!;
-                            homeCtrl.update();*/
-                          },
-                        );
-                      })
-                    ],
-                  ).marginOnly(left: Insets.i20),
-                ),
-
-                //recent View
-                CommonTitle(
-                  title: CommonFonts().womenCollection,
-                ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      ...geRecentProducts1.asMap().entries.map((e) {
-                        return RecentViewListCard(
-                          data: e.value,
-                          index: e.key,
-                          onTap: ()=> Get.toNamed(routeName.productDetail,arguments: e.value),
-                          heartIconTap: (){
-                           /* e.value.isFav = !e.value.isFav!;
-                            homeCtrl.update();*/
-                          },
-                        );
-                      })
-                    ],
-                  ).marginOnly(left: Insets.i20),
-                ),
-              ],
-            ).marginOnly(bottom: Insets.i50),
-          ),
-        );
-      }
-    );
+          ).marginOnly(bottom: Insets.i50),
+        ),
+      );
+    });
   }
 }
